b95d990752b7fcd565fe7bc04abc02d863364e1c
RichoKD <jerrykaduma@gmail.com>
2026-06-23 17:11:29 +0100

    generate addresses and transfer btc

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,8 @@
 #![allow(unused)]
+use bitcoin::address::NetworkUnchecked;
 use bitcoin::hex::DisplayHex;
-use bitcoincore_rpc::bitcoin::Amount;
+use bitcoin::Address;
+use bitcoincore_rpc::bitcoin::{Amount, Network};
 use bitcoincore_rpc::{Auth, Client, RpcApi};
 use serde::Deserialize;
 use serde_json::json;
@@ -15,13 +17,13 @@ const RPC_PASS: &str = "password";
 // You can use calls not provided in RPC lib API using the generic `call` function.
 // An example of using the `send` RPC call, which doesn't have exposed API.
 // You can also use serde_json `Deserialize` derivation to capture the returned json result.
-fn send(rpc: &Client, addr: &str) -> bitcoincore_rpc::Result<String> {
+fn send(rpc: &Client, addr: &str, amount: f64) -> bitcoincore_rpc::Result<String> {
     let args = [
-        json!([{addr : 100 }]), // recipient address
-        json!(null),            // conf target
-        json!(null),            // estimate mode
-        json!(null),            // fee rate in sats/vb
-        json!(null),            // Empty option object
+        json!([{addr : amount }]), // recipient address + amount
+        json!(null),               // conf target
+        json!(null),               // estimate mode
+        json!(null),               // fee rate in sats/vb
+        json!(null),               // Empty option object
     ];
 
     #[derive(Deserialize)]
@@ -50,12 +52,53 @@ fn wallet_loader(rpc: &Client, wallet_name: &str) -> bitcoincore_rpc::Result<Cli
     )?)
 }
 
+fn check_wallets() {
+    let rpc = Client::new(
+        // RPC_URL,
+        &format!("{}/wallet/{}", RPC_URL, "Trader"),
+        Auth::UserPass(RPC_USER.to_owned(), RPC_PASS.to_owned()),
+    )
+    .unwrap();
+
+    let received = rpc
+        .list_received_by_address(None, Some(1), None, None)
+        .unwrap();
+    for item in received {
+        println!("Received Address: {:?}", item.address);
+    }
+
+    let rpc = Client::new(
+        // RPC_URL,
+        &format!("{}/wallet/{}", RPC_URL, "Miner"),
+        Auth::UserPass(RPC_USER.to_owned(), RPC_PASS.to_owned()),
+    )
+    .unwrap();
+
+    let received = rpc
+        .list_received_by_address(None, Some(1), None, None)
+        .unwrap();
+    for item in received {
+        println!("Received Address: {:?}", item.address);
+    }
+}
+
 fn main() -> bitcoincore_rpc::Result<()> {
+    // check_wallets();
+    // return Ok(());
+
     // Connect to Bitcoin Core RPC
     let rpc = Client::new(
         RPC_URL,
+        // &format!("{}/wallet/{}", RPC_URL, "Trader"),
         Auth::UserPass(RPC_USER.to_owned(), RPC_PASS.to_owned()),
     )?;
+
+    // let received = rpc
+    //     .list_received_by_address(None, Some(1), None, None)
+    //     .unwrap();
+    // for item in received {
+    //     println!("Received Address: {:?}", item.address);
+    // }
 
     // Get blockchain info
     // let blockchain_info = rpc.get_blockchain_info()?;
@@ -69,12 +112,33 @@ fn main() -> bitcoincore_rpc::Result<()> {
     println!("Wallets: {:?}", wallets);
 
     // Generate spendable balances in the Miner wallet. How many blocks needs to be mined?
+    let miner_address = miner
+        .get_new_address(None, None)
+        .unwrap()
+        .require_network(Network::Regtest)
+        .unwrap();
+
+    println!("miner_address {} ", miner_address);
+
+    // miner.generate_to_address(103, &miner_address)?;
+
+    println!("Miner bal {} ", miner.get_balance(None, None)?);
 
     // Load Trader wallet and generate a new address
+    let trader_address = trader
+        .get_new_address(None, None)
+        .unwrap()
+        .require_network(Network::Regtest)
+        .unwrap();
+
+    println!("trader_address {} ", trader_address);
 
     // Send 20 BTC from Miner to Trader
+    let tx_id = send(&miner, &trader_address.to_string(), 20.0)?;
 
     // Check transaction in mempool
+
+    println!("trader bal {} ", trader.get_balance(None, None)?);
 
     // Mine 1 block to confirm the transaction
 
